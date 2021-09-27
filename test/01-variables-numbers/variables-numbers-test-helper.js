2e711c1f19c6b28f4d865748ec69afb4e93e9b8b
Matina <matinaspatsos@gmail.com>
2021-09-27 07:32:22 -0400

    Variables and numbers exercise

---
+++ test/01-variables-numbers/variables-numbers-test-helper.js
@@ -0,0 +1,40 @@
+import { getAnswer } from "../getAnswer.js";
+
+const myNameStr = getAnswer("../exercises/01-variables-numbers/01-my-name.js");
+const fixErrorStr = getAnswer(
+  "../exercises/01-variables-numbers/02-fix-error.js"
+);
+const addStr = getAnswer("../exercises/01-variables-numbers/03-add.js");
+
+const incrementedAgeByAYear = getAnswer(
+  "../exercises/01-variables-numbers/04-age-me-by-a-year.js"
+);
+
+const decrementAgeByAYearAnswer = getAnswer(
+  "../exercises/01-variables-numbers/05-make-me-younger-by-a-year.js"
+);
+
+export const myName = eval(`() => {
+  ${myNameStr}
+  return myName;
+}`);
+
+export const fixError = eval(`() => {
+  ${fixErrorStr}
+  return numberOfLikes;
+}`);
+
+export const add = eval(`(a, b) => {
+  ${addStr}
+  return sum;
+}`);
+
+export const ageMeByAYear = eval(`(age)=>{
+  ${incrementedAgeByAYear}
+  return age;
+}`);
+
+export const makeMeYoungerByAYear = eval(`(age)=>{
+  ${decrementAgeByAYearAnswer}
+  return age;
+}`);
