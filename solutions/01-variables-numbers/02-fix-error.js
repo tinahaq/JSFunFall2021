181a814e643ad5471a4d53737779b47928cab89b
Matina <matinaspatsos@gmail.com>
2021-10-06 18:01:30 -0400

    Variable and numbers solutions

---
+++ solutions/01-variables-numbers/02-fix-error.js
@@ -0,0 +1,11 @@
+/**
+ * There is an error with this code. Fix the error by changing the code where instructed below.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
+
+let numberOfLikes = 57; // Change me
+
+// DO NOT CHANGE THE CODE BELOW
+
+numberOfLikes = 58;
