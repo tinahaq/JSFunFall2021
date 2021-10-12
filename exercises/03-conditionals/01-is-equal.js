37e7b711cd247ae0904ebf147e1f33ca3dac5fd8
Matina Patsos <matinaspatsos@gmail.com>
2021-10-12 19:59:16 -0400

    Conditionals exercises

---
+++ exercises/03-conditionals/01-is-equal.js
@@ -0,0 +1,12 @@
+let userInput1 = 39; // e,g,
+let userInput2 = "39"; // e,g,
+
+/**
+ * Create a variable called "isEqual". (Do not use var.)
+ * If "userInput1" strictly equals "userInput2", "isEqual" should be true.
+ * Otherwise, it should be false.
+ * Hint: What does strictly mean?
+ * Your answer should still work when "userInput1" and "userInput2" are different values.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
