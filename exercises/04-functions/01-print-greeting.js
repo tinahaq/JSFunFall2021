e28bb9e4ec25b303af36377cf308e649487d200a
Matina Patsos <matinaspatsos@gmail.com>
2021-10-17 08:24:26 -0400

    Functions exercise

---
+++ exercises/04-functions/01-print-greeting.js
@@ -0,0 +1,8 @@
+/**
+ * Create a function named called "printGreeting".
+ * It should accept a name and return "Hello ______!"
+ * @example printGreeting("Tim"); // Hello Tim!
+ * You should use arrow syntax.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
---
+++ exercises/04-functions/02-times-five.js
@@ -0,0 +1,9 @@
+/**
+ * Create a function named "timesFive". It should multiply a number by 5.
+ * @example timesFive(5); // 25
+ * @example timesFive(2); // 10
+ * @example timesFive(0); // 0
+ * You should use arrow syntax.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
