f0c75c68b430f1cdc6123259a865f8893222e7ce
Matina <matinaspatsos@gmail.com>
2021-11-07 12:10:48 -0500

    Latest arrays, sets and objects solutions

---
+++ solutions/06-objects/02-create-nested-object.solution.js
@@ -0,0 +1,49 @@
+/**
+ * Complete the "createGraduateObject" function so that it takes in several arguments and combines them together in one object.
+ *
+ * @param {string} name
+ * @param {number} graduationYear
+ * @param {array} skills
+ * @param {string} githubLink
+ * @param {string} linkedInLink
+ *
+ * @returns {Object} with the properties "name", "graduationYear", "skills" and "links".
+ * "links" should be an object with the properties "github" and "linkedIn".
+ *
+ * @example
+ * const graduateObject = createGraduateObject("Eddie Willard", 2020, [ "Javascript", "React", "CSS" ], "https://github.com/example/profile", "https://linkedin.com/profile");
+ *
+ * console.log(graduateObject); //
+ * {
+ *   name: "Eddie Willard",
+ *   graduationYear: 2021,
+ *   skills: ["JavaScript", "React", "CSS"],
+ *   links: {
+ *     github: "https://github.com/example/profile",
+ *     linkedIn: "https://linkedin.com/profile"
+ *   }
+ * }
+ */
+
+const createGraduateObject = (
+  name,
+  graduationYear,
+  skills,
+  githubLink,
+  linkedInLink
+) => {
+  // WRITE YOUR ANSWER HERE
+  return {
+    name,
+    graduationYear,
+    skills,
+    links: {
+      github: githubLink,
+      linkedIn: linkedInLink,
+    },
+  };
+};
+
+// IGNORE THIS BELOW. It is for the tests.
+
+export default createGraduateObject;
